22357db53add7f5b37bc2cdd109adc14d546f901
Lawrence Martin <lawrence.martin@cantire.com>
2017-02-08 20:27:10 -0500

    Initial Commit

---
+++ NoFilter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NoFilter
+//
+//  Created by Lawrence Martin on 2017-02-08.
+//  Copyright © 2017 mapd.centennial.proapptive. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
