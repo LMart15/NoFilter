103203609ad43ee74ab46e33634addd4f19ab77c
Basil <basiltalias92@gmail.com>
2017-02-15 21:32:21 -0500

    Added Register view screen too

--- NoFilter/RegisterScreenView.swift
+++ NoFilter/RegisterScreenView.swift
@@ -7,17 +7,49 @@
 //
 
 import UIKit
+import Firebase
 
-class RegisterScreenView: UIViewController {
+class RegisterScreenView: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var fullname: UITextField!
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var confirmpassword: UITextField!
+    @IBOutlet weak var phonenumber: UITextField!
+    
+    let picker = UIImagePickerController()
+    
+    @IBAction func AddPhoto(_ sender: Any) {
+        picker.allowsEditing = true
+        picker.sourceType = .photoLibrary
+        present(picker, animated: true, completion: nil)
+    }
+    @IBAction func done(_ sender: Any) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        profileImg.layer.cornerRadius = 25;
+        profileImg.layer.masksToBounds = true
+        picker.delegate = self
+       
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = (info[UIImagePickerControllerEditedImage] as? UIImage)  {
+            self.profileImg.image = image
+            
+        }else {
+            print("Something went wrong")
+        }
+        
     }
 
 }
