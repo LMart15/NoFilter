7ba16358ce39b1e89a7de7adb7da53d7408915f0
Basil <basiltalias92@gmail.com>
2017-02-15 20:31:42 -0500

    Implemented the registration page layout

--- NoFilter/RegisterScreenView.swift
+++ NoFilter/RegisterScreenView.swift
@@ -1,34 +1,23 @@
 //
-//  ViewController.swift
+//  RegisterScreenView.swift
 //  NoFilter
 //
-//  Created by Lawrence Martin on 2017-02-08.
+//  Created by Basil on 2017-02-15.
 //  Copyright © 2017 mapd.centennial.proapptive. All rights reserved.
 //
 
 import UIKit
-import FirebaseAuth
 
-class ViewController: UIViewController {
+class RegisterScreenView: UIViewController {
 
-    @IBOutlet weak var username_label: UITextField!
-    @IBOutlet weak var password: UITextField!
-    
-    @IBAction func signIn(_ sender: Any) {
-    }
-    
-    @IBAction func signUp(_ sender: Any) {
-    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
 }
-
