06aac2273486e6f0cee8124f4d69a45040887d7a
happy <jaismeen.sandhu@gmail.com>
2017-03-27 21:46:27 -0400

    first commit

---
+++ NoFilter/AudioCommentCell.swift
@@ -0,0 +1,24 @@
+//
+//  AudioCommentCell.swift
+//  NoFilter
+//
+//  Created by harpreet singh on 2017-03-26.
+//  Copyright © 2017 mapd.centennial.proapptive. All rights reserved.
+//
+
+import UIKit
+
+class AudioCommentCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
